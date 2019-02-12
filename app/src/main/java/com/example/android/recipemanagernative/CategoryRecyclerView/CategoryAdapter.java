85ef40f20f3f455388f353164c993abf6e1ff9fc
George <gwalter246@hotmail.co.uk>
2019-02-12 18:16:42 +0000

    Integrated recycler view with database

--- app/src/main/java/com/example/android/recipemanagernative/CategoryRecyclerView/CategoryAdapter.java
+++ app/src/main/java/com/example/android/recipemanagernative/CategoryRecyclerView/CategoryAdapter.java
@@ -1,20 +1,21 @@
 package com.example.android.recipemanagernative.CategoryRecyclerView;
 
+import android.content.Context;
+import android.database.Cursor;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.example.android.recipemanagernative.Database.RecipeManagerContract;
 import com.example.android.recipemanagernative.R;
-
-import java.util.List;
 
 public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.CategoryViewHolder> {
 
-    private List<Category> categoryList;
+    private Context context;
+    private Cursor cursor;
 
     // Provides a reference to the views for each data item.
     public static class CategoryViewHolder extends RecyclerView.ViewHolder {
@@ -27,32 +28,52 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.Catego
     }
 
     // Constructor for the CategoryAdapter class.
-    public CategoryAdapter(List<Category> categoryList){
-        this.categoryList = categoryList;
+    public CategoryAdapter(Context context, Cursor cursor){
+        this.context = context;
+        this.cursor = cursor;
     }
 
     // Creates new views.
     @Override
     @NonNull
     public CategoryAdapter.CategoryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-
-        // Inflates the view for the list row.
-        View categoryRowView = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.category_row, parent, false);
-
+        View categoryRowView = LayoutInflater.from(this.context).inflate(R.layout.category_row, parent, false);
         return new CategoryViewHolder(categoryRowView);
     }
 
     // Replace the contents of a view.
     @Override
     public void onBindViewHolder(@NonNull CategoryViewHolder holder, int position) {
-        Category category = categoryList.get(position);
-        holder.categoryNameTextView.setText(category.getCategoryName());
+        // Checks the cursor can move to the position.
+        if(!cursor.moveToPosition(position)){
+            return;
+        }
+
+        // Gets the category name from the row in the cursor.
+        String categoryName = cursor.getString(cursor.getColumnIndex(RecipeManagerContract.CategoryEntry.COLUMN_CATEGORY_NAME));
+        holder.categoryNameTextView.setText(categoryName);
     }
 
     // Returns the size of the dataset.
     @Override
     public int getItemCount() {
-        return categoryList.size();
+        return cursor.getCount();
+    }
+
+    // Updates the cursor with new data.
+    public void updateCursor(Cursor newCursor){
+
+        // Closes the cursor if it is not empty.
+        if(cursor != null) {
+            cursor.close();
+        }
+
+        // Assigns the new cursor.
+        cursor = newCursor;
+
+        // Refreshes the recycler view if the cursor is not empty.
+        if(newCursor != null) {
+            notifyDataSetChanged();
+        }
     }
 }
--- app/src/main/java/com/example/android/recipemanagernative/MainActivity.java
+++ app/src/main/java/com/example/android/recipemanagernative/MainActivity.java
@@ -1,39 +1,22 @@
 package com.example.android.recipemanagernative;
 
-import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.inputmethod.EditorInfo;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.android.recipemanagernative.CategoryRecyclerView.CategoryAdapter;
-import com.example.android.recipemanagernative.Database.RecipeManagerContract;
 import com.example.android.recipemanagernative.Database.RecipeManagerDbHelper;
-import com.example.android.recipemanagernative.CategoryRecyclerView.Category;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RecyclerView categoryRecyclerView;
-    private RecyclerView.Adapter categoryAdapter;
-    private RecyclerView.LayoutManager layoutManager;
-
-    // GUT
-    private List<Category> categoryList = new ArrayList<Category>();
+    CategoryAdapter categoryAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,28 +27,21 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar_main);
         setSupportActionBar(toolbar);
 
-        /*
-        // Creates a recycler view.
-        categoryRecyclerView = (RecyclerView) findViewById(R.id.category_recycler_view);
+        // Creates the recycler view.
+        RecyclerView categoryRecyclerView = (RecyclerView) findViewById(R.id.category_recycler_view);
+        categoryRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        categoryAdapter = new CategoryAdapter(this, getAllCategories());
+        categoryRecyclerView.setAdapter(categoryAdapter);
 
         // Adds a separator to the recycler view.
         categoryRecyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
 
-        // Creates a layout manager.
-        layoutManager = new LinearLayoutManager(this);
+    }
 
-        updateRecyclerView();
-
-        // Creates a recycler view adapter.
-        categoryAdapter = new CategoryAdapter(categoryList);
-
-        // Configures the recycler view.
-        categoryRecyclerView.setAdapter(categoryAdapter);
-        categoryRecyclerView.setLayoutManager(layoutManager);*/
-
-        // Gets the database.
-        SQLiteDatabase db = RecipeManagerDbHelper.getInstance(this).getReadableDatabase();
-
+    @Override
+    protected void onStart(){
+        super.onStart();
+        categoryAdapter.updateCursor(getAllCategories());
     }
 
     @Override
@@ -90,28 +66,8 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-    }
-
-    private void updateRecyclerView(){
-        categoryList.clear();
-        Cursor cursor = RecipeManagerDbHelper.getInstance(this).readCategory();
-        int categoryNameColumnIndex = cursor.getColumnIndex(RecipeManagerContract.CategoryEntry.COLUMN_CATEGORY_NAME);
-
-        try{
-            while(cursor.moveToNext()){
-                String categoryName = cursor.getString(categoryNameColumnIndex);
-                categoryList.add(new Category(categoryName));
-            }
-        }
-        catch (Exception e){
-            Toast.makeText(this, "Error reading database",Toast.LENGTH_SHORT).show();
-        }
-        finally {
-            cursor.close();
-        }
+    private Cursor getAllCategories() {
+        return RecipeManagerDbHelper.getInstance(this).readCategory();
     }
 }
 
